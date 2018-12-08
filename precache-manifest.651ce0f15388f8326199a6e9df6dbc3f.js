899b03d73a0e771078f66ffa15ebbd9ebcf95b44
Umut Benzer <umutbenzer@gmail.com>
2018-12-08 15:02:04 +0100

    Updates

---
+++ precache-manifest.651ce0f15388f8326199a6e9df6dbc3f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "aae02d7af944668d1041",
+    "url": "/sidenavjs/static/js/main.aae02d7a.chunk.js"
+  },
+  {
+    "revision": "f2fe6720256f846435f0",
+    "url": "/sidenavjs/static/js/1.f2fe6720.chunk.js"
+  },
+  {
+    "revision": "c45aade585b5482d9aa1",
+    "url": "/sidenavjs/static/js/runtime~main.c45aade5.js"
+  },
+  {
+    "revision": "cd4645f078575d25100b1e8d15335093",
+    "url": "/sidenavjs/index.html"
+  }
+];
