60abdd1620d3d899c7c8450401bab8b2beec9c7f
Umut Benzer <umutbenzer@gmail.com>
2019-08-31 01:15:49 +0200

    Updates

---
+++ precache-manifest.4ed883ff280af000d71758e0305f1a5b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f66cabfc6b75653f5f7fbf0982d09ae",
+    "url": "/sidenavjs/index.html"
+  },
+  {
+    "revision": "91f9588b6e3132bb45d6",
+    "url": "/sidenavjs/static/js/2.2f8e09e7.chunk.js"
+  },
+  {
+    "revision": "b0e9fe8887e9339515df",
+    "url": "/sidenavjs/static/js/main.ec09a9af.chunk.js"
+  },
+  {
+    "revision": "cec6de095ca8f7508a9c",
+    "url": "/sidenavjs/static/js/runtime~main.56b65aa6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sidenavjs/precache-manifest.651ce0f15388f8326199a6e9df6dbc3f.js"
+  "/sidenavjs/precache-manifest.4ed883ff280af000d71758e0305f1a5b.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/sidenavjs/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sidenavjs/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
